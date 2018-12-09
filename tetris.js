d176f86a5bd129a149ceb459e2aafe68224cac54
Seah Ying Xiang <seahyx@gmail.com>
2018-12-09 14:35:42 +0800

    Updated version number

--- tetris.js
+++ tetris.js
@@ -40,7 +40,7 @@ window.addEventListener("resize", function(event){
 let DEBUG_MODE = false;
 
 //Version number
-const VERSION = "1.0.01";
+const VERSION = "1.1.0";
 console.log("Tetris version " + VERSION);
 
 //Grid details
